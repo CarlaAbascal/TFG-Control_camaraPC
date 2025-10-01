a22ac2e9f144d4ba03ec95a57e3bab8ca5ecc75d
CarlaAbascal <carla.abascal@estudiantat.upc.edu>
2025-10-01 21:57:31 +0200

    version 1.3

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -32,6 +32,13 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.pictureBoxPC = new System.Windows.Forms.PictureBox();
+            this.pictureBoxDron = new System.Windows.Forms.PictureBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPC)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDron)).BeginInit();
             this.SuspendLayout();
             // 
             // altLbl
@@ -79,11 +86,68 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // pictureBoxPC
+            // 
+            this.pictureBoxPC.Location = new System.Drawing.Point(275, 38);
+            this.pictureBoxPC.Name = "pictureBoxPC";
+            this.pictureBoxPC.Size = new System.Drawing.Size(264, 128);
+            this.pictureBoxPC.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxPC.TabIndex = 5;
+            this.pictureBoxPC.TabStop = false;
+//            this.pictureBoxPC.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBoxDron
+            // 
+            this.pictureBoxDron.Location = new System.Drawing.Point(275, 195);
+            this.pictureBoxDron.Name = "pictureBoxDron";
+            this.pictureBoxDron.Size = new System.Drawing.Size(264, 128);
+            this.pictureBoxDron.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxDron.TabIndex = 6;
+            this.pictureBoxDron.TabStop = false;
+//            this.pictureBoxDron.Click += new System.EventHandler(this.pictureBoxDron_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.SlateGray;
+            this.button4.Location = new System.Drawing.Point(275, 331);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(264, 48);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Streaming video";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(438, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 20);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Video PC";
+//            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(438, 184);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 20);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Video Dron";
+//            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(427, 450);
+            this.ClientSize = new System.Drawing.Size(640, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.pictureBoxDron);
+            this.Controls.Add(this.pictureBoxPC);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -91,7 +155,10 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPC)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDron)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -101,6 +168,11 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBoxPC;
+        private System.Windows.Forms.PictureBox pictureBoxDron;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using csDronLink;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
 
 namespace WindowsFormsApp1
 {
@@ -15,18 +14,22 @@ namespace WindowsFormsApp1
     {
         Dron miDron = new Dron();
 
+        // Variables para streaming
+        private VideoCapture capPC;
+        private VideoCapture capDron;
+        private bool running = false;
+
         public Form1()
         {
             InitializeComponent();
-            CheckForIllegalCrossThreadCalls = false; // Para evitar problemas de hilos con los labels
+            CheckForIllegalCrossThreadCalls = false;
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            
         }
 
-       // Recorrer la lista con los datos de telemetría y mostrar la altitud en un label
+        // --------------------------TELEMETRÍA--------------------------
         private void ProcesarTelemetria(byte id, List<(string nombre, float valor)> telemetria)
         {
             foreach (var t in telemetria)
@@ -60,8 +63,142 @@ namespace WindowsFormsApp1
 
         private void button3_Click(object sender, EventArgs e)
         {
-            miDron.Aterrizar();
+            miDron.Aterrizar(bloquear: false);
         }
 
+        // --------------------------STREAMING--------------------------
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // Abrir cámara del PC
+            capPC = new VideoCapture(0);
+
+            // Abrir streaming del dron
+            capDron = new VideoCapture("tcp://127.0.0.1:57600/live");
+
+            running = true;
+
+            Task.Run(() =>
+            {
+                Mat matPC = new Mat();
+                Mat matDron = new Mat();
+
+                while (running)
+                {
+                    // Frame de la cámara del PC
+                    if (capPC.Read(matPC) && !matPC.Empty())
+                    {
+                        string gesture = DetectGesture(matPC);
+                        if (!string.IsNullOrEmpty(gesture))
+                        {
+                            // Usamos OpenCvSharp.Point explícitamente
+                            Cv2.PutText(matPC, $"Gesto: {gesture}", new OpenCvSharp.Point(30, 30),
+                                HersheyFonts.HersheySimplex, 1.0, Scalar.Red, 2);
+
+                            ExecuteGestureAction(gesture);
+                        }
+
+                        pictureBoxPC.Image?.Dispose();
+                        pictureBoxPC.Image = BitmapConverter.ToBitmap(matPC);
+                    }
+
+                    // Frame del dron
+                    if (capDron.Read(matDron) && !matDron.Empty())
+                    {
+                        pictureBoxDron.Image?.Dispose();
+                        pictureBoxDron.Image = BitmapConverter.ToBitmap(matDron);
+                    }
+                }
+            });
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            running = false;
+            capPC?.Release();
+            capDron?.Release();
+            base.OnFormClosing(e);
+        }
+
+        // --------------------------DETECCIÓN DE GESTOS--------------------------
+        private string DetectGesture(Mat frame)
+        {
+            if (frame.Empty()) return null;
+
+            Mat hsv = new Mat();
+            Cv2.CvtColor(frame, hsv, ColorConversionCodes.BGR2HSV);
+
+            // Color piel (ajusta si no detecta bien)
+            Scalar lower = new Scalar(0, 30, 60);
+            Scalar upper = new Scalar(20, 150, 255);
+            Mat mask = new Mat();
+            Cv2.InRange(hsv, lower, upper, mask);
+
+            Cv2.GaussianBlur(mask, mask, new OpenCvSharp.Size(5, 5), 0);
+            Cv2.Threshold(mask, mask, 127, 255, ThresholdTypes.Binary);
+
+            Cv2.FindContours(mask, out OpenCvSharp.Point[][] contours, out _, RetrievalModes.External, ContourApproximationModes.ApproxSimple);
+            if (contours.Length == 0) return null;
+
+            var cnt = contours.OrderByDescending(c => Cv2.ContourArea(c)).First();
+            if (Cv2.ContourArea(cnt) < 2000) return null;
+
+            int[] hull = Cv2.ConvexHullIndices(cnt);
+
+            if (hull.Length > 3)
+            {
+                var defects = Cv2.ConvexityDefects(cnt, hull);
+                int fingers = 0;
+                foreach (var defect in defects)
+                {
+                    OpenCvSharp.Point start = cnt[defect.Item0];
+                    OpenCvSharp.Point end = cnt[defect.Item1];
+                    OpenCvSharp.Point far = cnt[defect.Item2];
+
+                    double a = Math.Sqrt(Math.Pow(end.X - start.X, 2) + Math.Pow(end.Y - start.Y, 2));
+                    double b = Math.Sqrt(Math.Pow(far.X - start.X, 2) + Math.Pow(far.Y - start.Y, 2));
+                    double c = Math.Sqrt(Math.Pow(end.X - far.X, 2) + Math.Pow(end.Y - far.Y, 2));
+                    double angle = Math.Acos((b * b + c * c - a * a) / (2 * b * c));
+
+                    if (angle <= Math.PI / 2)
+                        fingers++;
+                }
+
+                if (fingers == 0) return "fist";      // puño -> aterrizar
+                if (fingers == 1) return "one";       // un dedo -> avanzar
+                if (fingers == 2) return "two";       // dos dedos -> derecha
+                if (fingers == 3) return "three";     // tres dedos -> izquierda
+                if (fingers >= 4) return "palm";      // palma abierta -> despegr
+            }
+
+            return null;
+        }
+
+        // --------------------------ACCIONES DEL DRON POR GESTO--------------------------
+        private void ExecuteGestureAction(string gesture)
+        {
+            switch (gesture)
+            {
+                case "palm": // mano abierta → despegar
+                    miDron.Despegar(20, bloquear: false, f: EnAire, param: "Volando");
+                    break;
+
+                case "fist": // puño → aterrizar
+                    miDron.Aterrizar(bloquear: false);
+                    break;
+
+                case "two": // dos dedos → girar derecha
+                    miDron.CambiarHeading(90, bloquear: false);
+                    break;
+
+                case "three": // tres dedos → girar izquierda
+                    miDron.CambiarHeading(270, bloquear: false);
+                    break;
+
+                case "one": // un dedo → avanzar
+                    miDron.Mover("Forward", 10, bloquear: false);
+                    break;
+            }
+        }
     }
 }
+
